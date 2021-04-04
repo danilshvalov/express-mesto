b42a5dcd0be9ef4bacdd448530f4e8631c288e55
Danil Shvalov <shvalovich@gmail.com>
2021-04-04 20:37:05 +1000

    Теперь /signin возвращает токен

--- controllers/user.js
+++ controllers/user.js
@@ -135,7 +135,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({message: 'Вы успешно авторизовались'});
+        .send({token});
     })
     .catch(next);
 };
